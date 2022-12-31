305d9f0adf57f6141b15840d3d37a48eeedb3e0e
Pier Francesco Contino <pfcontino@gmail.com>
2022-12-31 16:29:23 +0100

    refactor base_lib

--- src/base_lib.c
+++ src/base_lib.c
@@ -1,6 +1,6 @@
 #include "base_lib.h"
 
-void error_msg(char *msg)
+void error_msg(const char *msg)
 {
 	perror(msg);
 	exit(EXIT_FAILURE);
@@ -15,7 +15,7 @@ void *allocate(int size)
 	return p;
 }
 
-void get_arg(char *coded, unsigned long int *start, unsigned long int *inc,
+void get_arg(const char *coded, unsigned long *start, unsigned long *inc,
 	     unsigned int *size)
 {
 	if (sscanf(coded, "%lu:%lu:%u", start, inc, size) != 3)
@@ -28,34 +28,3 @@ void get_current_time(struct timespec *curr_t)
 		error_msg("error in gettime");
 }
 
-void timespec_diff(struct timespec *start, struct timespec *stop,
-		   struct timespec *result)
-{
-	if ((stop->tv_nsec - start->tv_nsec) < 0) {
-		result->tv_sec = stop->tv_sec - start->tv_sec - 1;
-		result->tv_nsec =
-		    stop->tv_nsec - start->tv_nsec + TIME_PRECISION;
-	} else {
-		result->tv_sec = stop->tv_sec - start->tv_sec;
-		result->tv_nsec = stop->tv_nsec - start->tv_nsec;
-	}
-}
-
-void timespec_div(struct timespec *rop, struct timespec *op, unsigned int div)
-{
-	rop->tv_nsec =
-	    ((op->tv_sec % div) * TIME_PRECISION / div) + (op->tv_nsec / div);
-	rop->tv_sec = op->tv_sec / div;
-}
-
-void timespec_sum(struct timespec *rop, struct timespec *op1,
-		  struct timespec *op2)
-{
-	if (op1->tv_nsec + op2->tv_nsec >= TIME_PRECISION) {
-		rop->tv_sec = op1->tv_sec + op2->tv_sec + 1;
-		rop->tv_nsec = op1->tv_nsec + op2->tv_nsec - TIME_PRECISION;
-	} else {
-		rop->tv_sec = op1->tv_sec + op2->tv_sec;
-		rop->tv_nsec = op1->tv_nsec + op2->tv_nsec;
-	}
-}
--- src/base_lib.h
+++ src/base_lib.h
@@ -6,22 +6,46 @@
 #include <string.h>
 #include <time.h>
 
-//#define DEBUG
 #define MINIMAL
 #define TIME_PRECISION 1000000000
 
-void error_msg(char *msg);
+void error_msg(const char *msg);
 void *allocate(int size);
 
-void get_arg(char *coded, unsigned long int *start, unsigned long int *inc,
+void get_arg(const char *coded, unsigned long *start, unsigned long *inc,
 	     unsigned int *size);
 
 void get_current_time(struct timespec *curr_t);
-void timespec_diff(struct timespec *start, struct timespec *stop,
-		   struct timespec *result);
-void timespec_sum(struct timespec *rop, struct timespec *op1,
-		  struct timespec *op2);
-void timespec_div(struct timespec *rop, struct timespec *op, unsigned int div);
+
+static inline void timespec_div(struct timespec *rop, const struct timespec *op, unsigned int div)
+{
+	rop->tv_nsec =
+	    ((op->tv_sec % div) * TIME_PRECISION / div) + (op->tv_nsec / div);
+	rop->tv_sec = op->tv_sec / div;
+}
+
+static inline void timespec_sum(struct timespec *rop, const struct timespec *op1, const struct timespec *op2)
+{
+	if (op1->tv_nsec + op2->tv_nsec >= TIME_PRECISION) {
+		rop->tv_sec = op1->tv_sec + op2->tv_sec + 1;
+		rop->tv_nsec = op1->tv_nsec + op2->tv_nsec - TIME_PRECISION;
+	} else {
+		rop->tv_sec = op1->tv_sec + op2->tv_sec;
+		rop->tv_nsec = op1->tv_nsec + op2->tv_nsec;
+	}
+}
+
+static inline void timespec_diff(const struct timespec *start, const struct timespec *stop, struct timespec *result)
+{
+	if ((stop->tv_nsec - start->tv_nsec) < 0) {
+		result->tv_sec = stop->tv_sec - start->tv_sec - 1;
+		result->tv_nsec =
+		    stop->tv_nsec - start->tv_nsec + TIME_PRECISION;
+	} else {
+		result->tv_sec = stop->tv_sec - start->tv_sec;
+		result->tv_nsec = stop->tv_nsec - start->tv_nsec;
+	}
+}
 
 #define CHECK_ARGC(param_n) if (argc < (param_n + 1)) \
 do { \
