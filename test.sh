3056627a67aa8ec6d82a7e06c7b0e2b8f4872b63
Pier Francesco Contino <pfcontino@gmail.com>
2023-01-02 17:13:18 +0100

    test is more rapid

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 path="./test/"
 ext_t=".csv"
 ext_f="_log.txt"
-n1="t_12"
+n1="t_11"
 fn1_t=${path}${n1}${ext_t}
 fn1_f=${path}${n1}${ext_f}
 echo "c,b1,b2,iter,time" > ${fn1_t}
 echo -e "n\tf1\tf2\tfase\titer" > ${fn1_f}
-./test.o fact 8 12 3000:1000:9 ${fn1_t} ${fn1_f}
+./test.o fact 8 11 3000:1000:9 ${fn1_t} ${fn1_f}
 
 cat ${fn1_f}
