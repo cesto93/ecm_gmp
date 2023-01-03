50b62bbd30da3fe26bfc699a0a18dca93c728e67
Pier Francesco Contino <pfcontino@gmail.com>
2023-01-03 10:50:48 +0100

    remove useless var

--- src/m_ellcurv_fact.c
+++ src/m_ellcurv_fact.c
@@ -27,18 +27,15 @@ long factorize(mpz_t factors[], const mpz_t n, unsigned long b1, unsigned long b
 
 long mfactorize(mpz_t factors[], const mpz_t n, unsigned long b1, unsigned long b2, unsigned long max_iter)
 {
-	const unsigned long n_size = mpz_size(n) * mp_bits_per_limb;
 	int fase_found = -1;
 	unsigned long iter_done = 0;
 	mm_fact_param param;
 	unsigned long seed;
 	gmp_randstate_t state;
-	mpz_t fact;
 
 	if (syscall(SYS_getrandom, &seed, sizeof(unsigned long), 0) == -1)
 		error_msg("error in getrandom at m_ell_fact\n");
 
-	mpz_init2(fact, n_size);
 	gmp_randinit_default(state);
 	gmp_randseed_ui(state, seed);
 
