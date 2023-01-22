102a863d09cd3736d2ee076a0cad56505b9ff0ce
Pier Francesco Contino <pfcontino@gmail.com>
2023-01-22 14:04:34 +0100

    bench: clearer script

--- bench.sh
+++ bench.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 path="./test/"
-n1="bench_20_29"
 ext_csv=".csv"
 ext_log="_time.csv"
 
-declare -A log csv bin
+start_iter=20
+end_iter=25
+sub_iter=1
 
+declare -A log csv bin
+declare -a b_values
+
+b_values[20]=8000:2000:${sub_iter}
+b_values[21]=15000:5000:${sub_iter}
+b_values[22]=20000:5000:${sub_iter}
+b_values[23]=30000:5000:${sub_iter}
+b_values[24]=45000:5000:${sub_iter}
+b_values[25]=50000:5000:${sub_iter}
+b_values[26]=10000:5000:${sub_iter}
+b_values[27]=150000:5000:${sub_iter}
+b_values[28]=20000:5000:${sub_iter}
+b_values[29]=25000:5000:${sub_iter}
+
+n1="bench_${start_iter}_${end_iter}"
 log[c]=${path}${n1}${ext_csv}
 csv[c]=${path}${n1}${ext_log}
 bin[c]="./test.o"
@@ -17,51 +33,21 @@ bin[wasm]="./test.wasm"
 
 log[mm]=${path}${n1}_mm${ext_csv}
 csv[mm]=${path}${n1}_mm${ext_log}
-bin[mm]="./mmtest"
+bin[mm]="./mmtest.o"
 
 run_bench() {
 	label=${1}
 	rep=10
+	local i
 
 	echo "c,b1,b2,iter,time" > ${csv["${label}"]}
 	echo -e "n\tf1\tf2\tfase\titer" > ${log["${label}"]}
 
-	${bin["${label}"]} fact ${rep} 20 8000:2000:3 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 21 15000:5000:3 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 22 20000:5000:3 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 23 30000:5000:3 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 24 45000:5000:3 ${csv["${label}"]} ${log["${label}"]}
-}
-
-run_benchs() {
-	label=${1}
-	rep=10
-
-	echo "c,b1,b2,iter,time" > ${csv["${label}"]}
-	echo -e "n\tf1\tf2\tfase\titer" > ${log["${label}"]}
-
-	${bin["${label}"]} fact ${rep} 20 8000:2000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 21 15000:5000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 22 20000:5000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 23 30000:5000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 24 45000:5000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 25 50000:5000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 26 100000:5000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 27 150000:5000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 28 200000:5000:1 ${csv["${label}"]} ${log["${label}"]}
-	${bin["${label}"]} fact ${rep} 29 250000:5000:1 ${csv["${label}"]} ${log["${label}"]}
+	for ((i = start_iter; i <= end_iter; i++)); do
+		${bin["${label}"]} fact ${rep} ${i} ${b_values[$i]} ${csv["${label}"]} ${log["${label}"]}
+	done
 }
 
 run_bench c
-run_bench wasm
-
-#${bin} fact 8 21 15000:5000:8 ${fn1_t} ${fn1_f}
-#${bin} fact 8 22 20000:5000:8 ${fn1_t} ${fn1_f}
-#${bin} fact 8 23 30000:5000:7 ${fn1_t} ${fn1_f}
-#${bin} fact 8 24 40000:5000:5 ${fn1_t} ${fn1_f}
-#${bin} fact 8 25 45000:5000:4 ${fn1_t} ${fn1_f}
-#${bin} fact 8 26 100000:25000:5 ${fn1_t} ${fn1_f}
-#${bin} fact 8 27 125000:25000:5 ${fn1_t} ${fn1_f}
-#${bin} fact 8 28 150000:25000:5 ${fn1_t} ${fn1_f}
-#${bin} fact 8 29 175000:25000:5 ${fn1_t} ${fn1_f}
-#${bin} fact 8 30 200000:25000:5 ${fn1_t} ${fn1_f}
+#run_bench wasm
+run_bench mm
